6444f8489fa11519566661f0dd47a9883a2dca86
kaystar <k.a.wood91@gmail.com>
2019-05-04 22:31:23 -0400

    Chapter 6 of "Python Crash Course"

---
+++ nesting_dicts.py
@@ -0,0 +1,8 @@
+alien_0 = {'colour' : 'green', 'points' : 5}
+alien_1 = {'colour' : 'pink', 'points' : 10}
+alien_2 = {'colour' : 'blue', 'points' : 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(aliens)
