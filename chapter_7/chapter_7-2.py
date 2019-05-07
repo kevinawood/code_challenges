997a51a5f89e12314e575c5c87be6937a7d00bc5
kaystar <k.a.wood91@gmail.com>
2019-05-06 21:50:32 -0400

    Chapter 7 of "Python Crash Course"

---
+++ chapter_7/chapter_7-2.py
@@ -0,0 +1,8 @@
+# Restaurant Seating
+
+guests = int(input('Welcome, how many guests are with you tonight?\n:'))
+
+if guests >= 8:
+    print("I'm sorry, you'll have to wait for a table")
+else:
+    print('I have a table ready for you')
