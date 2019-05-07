cc78ee44b9131c3cd2a4d21de8dba38f68c25e49
kaystar <k.a.wood91@gmail.com>
2019-05-06 21:43:03 -0400

    Chapter 7 of "Python Crash Course"

--- myDjangoEnv/looping_dicts.py
+++ myDjangoEnv/looping_dicts.py
@@ -1,14 +1,14 @@
-# user_0 = {
-#     'username': 'KR11_HOTH',
-#     'first': 'Kevin',
-#     'last': 'Anaro-Wood',
-# }
+user_0 = {
+    'username': 'KR11_HOTH',
+    'first': 'Kevin',
+    'last': 'Anaro-Wood',
+}
 #
 # # print(user_0['first'])
 # for key, value in user_0.items():
 #     print("\nKey: {}".format(key))
 #     print("Value: {}".format(value))
-
+#
 favourite_languages = {
     'Kevin' : 'Python',
     'James' : 'C',
@@ -16,8 +16,8 @@ favourite_languages = {
     'Aunty' : 'Perl',
 }
 
-for name, language in favourite_languages.items():
-    print("{}'s favourite language is {}".format(name.title(), language.title()))
+# for name, language in favourite_languages.items():
+#     print("{}'s favourite language is {}".format(name.title(), language.title()))
 
 # for name in favourite_languages.keys():
 #     print("{}".format(name.title()))
@@ -25,9 +25,39 @@ for name, language in favourite_languages.items():
 # for name in favourite_languages:
 #     print("{}".format(name.title()))
 
-friends = ['Kevin', 'Aunty']
-for name in favourite_languages:
-    print(name.title())
+# # Loop through each value in dictionary, if present and in our list of friends. Print special message
+# friends = ['Kevin', 'Aunty']
+# for name in favourite_languages:
+#     print(name.title())
+#
+#     if name in friends:
+#         print("Hey {}, I see your favourite language is {}".format(name, favourite_languages[name].title()))
 
-    if name in friends:
-        print("Hey {}, I see your favourite language is {}".format(name, favourite_languages[name].title()))
+# if 'erin' not in favourite_languages:
+#     print('Erin please take our poll')
+
+# # Print dictionary in order
+# for name in sorted(favourite_languages):
+#     print("{}".format(name))
+#
+# # Print dictionary with DISTINCT values
+# for language in set(favourite_languages.values()):
+#     print("{}".format(language))
+#
+# for key in favourite_languages:
+#     print('{}'.format(key[key]))
+
+rivers = {
+    'nile' : 'egypt',
+    'thames' : 'england',
+    'amazon' : 'brazil',
+}
+
+for river in rivers:
+    print("The river {} runs through {}".format(river.title(), rivers[river].title()))
+
+for river in rivers:
+    print(river)
+
+for river in rivers:
+    print(rivers[river].title())
