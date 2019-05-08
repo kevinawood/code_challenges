567f3ae11d284a880467a864e0544afd7cdabe84
kaystar <k.a.wood91@gmail.com>
2019-05-07 21:23:49 -0400

    Chapter 7 of "Python Crash Course"

--- chapter_7/exercise_7-8.py
+++ chapter_7/exercise_7-8.py
@@ -1,7 +1,10 @@
 # Deli Sandwiches
 
-sandwich_orders = ['Deli', 'Ham', 'Tuna', 'Italiano']
+sandwich_orders = ['Deli', 'pastrami', 'Ham', 'Tuna', 'pastrami', 'Italiano', 'pastrami']
 finished_orders = []
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
 
 for sandwich in sandwich_orders:
     print('I made you a {} sandwich\n'.format(sandwich))
